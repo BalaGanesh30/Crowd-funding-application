d50f869525998684532553007d287c369b1d83fc
BalaGanesh <135745561+BalaGanesh30@users.noreply.github.com>
2025-05-22 14:55:46 +0530

    Update api.js

--- client/uitiles/api.js
+++ client/uitiles/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/v1",
+  // baseURL: "http://localhost:8080/v1",
+   baseURL: "https://crowd-funding-application-server.onrender.com/v1",
 });
 
 // Attach token to requests
